34df5012fb86be82999c3c7b0ed79dc77069da3d
Holger Woltersdorf <hw@hollo.me>
2017-09-03 19:26:03 +0200

    Adds basic auth until go-live

--- web/index.php
+++ web/index.php
@@ -1,13 +1,27 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use OpenCFP\Application;
 use OpenCFP\Environment;
 
-$basePath = realpath(dirname(__DIR__));
+$basePath    = realpath( dirname( __DIR__ ) );
 $environment = Environment::fromEnvironmentVariable();
 
-$app = new Application($basePath, $environment);
+if ( $environment->equals( Environment::production() ) )
+{
+	$authUser     = $_SERVER['PHP_AUTH_USER'] ?? '';
+	$authPassword = $_SERVER['PHP_AUTH_PW'] ?? '';
+
+	if ( $authUser !== 'PHPDD18' || $authPassword !== 'PreView' )
+	{
+		header( 'WWW-Authenticate: Basic realm="Login"' );
+		header( 'HTTP/1.0 401 Unauthorized' );
+		echo 'Unauthorized';
+		exit;
+	}
+}
+
+$app = new Application( $basePath, $environment );
 
 $app->run();
