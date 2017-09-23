16ff9b5e15cb17b925269ce4826baa0f20946aac
Holger Woltersdorf <hw@hollo.me>
2017-09-23 19:42:20 +0200

    removed basic auth

--- config/BasicAuth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-	'user'     => 'PHPDD18',
-	'password' => 'PreView',
-];
--- web/index.php
+++ web/index.php
@@ -8,21 +8,6 @@ use OpenCFP\Environment;
 $basePath    = realpath( dirname( __DIR__ ) );
 $environment = Environment::fromEnvironmentVariable();
 
-if ( $environment->equals( Environment::production() ) )
-{
-	$authData     = require __DIR__ . '/../config/BasicAuth.php';
-	$authUser     = $_SERVER['PHP_AUTH_USER'] ?? '';
-	$authPassword = $_SERVER['PHP_AUTH_PW'] ?? '';
-
-	if ( $authUser !== $authData['user'] || $authPassword !== $authData['password'] )
-	{
-		header( 'WWW-Authenticate: Basic realm="Login"' );
-		header( 'HTTP/1.0 401 Unauthorized' );
-		echo 'Unauthorized';
-		exit;
-	}
-}
-
 $app = new Application( $basePath, $environment );
 
 $app->run();
